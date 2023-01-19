75634f124a636b2343ebd7dd9911bf545b19919b
PL97 <pengle@126.com>
2023-01-18 22:56:28 -0600

    update run command

--- task/NER/bash_scripts/fed.sh
+++ task/NER/bash_scripts/fed.sh
@@ -2,11 +2,11 @@
 
 
 # CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=$2 python fed_main.py saved_dir $1
-workspace="workspace_medical_2018_challenge_$3_split_test"
+workspace="workspace_medical_2018_challenge_$3_split_$4"
 rm -rf $workspace/$1
 mkdir $workspace/
 mkdir $workspace/$1
 CUDA_VISIBLE_DEVICES=$2 python fed_main.py \
     --workspace $workspace/$1 \
     --n_split $3 \
-    > $workspace/$1/log.txt
+    --model $4 > $workspace/$1/log.txt
--- task/NER/bash_scripts/run.sh
+++ task/NER/bash_scripts/run.sh
@@ -2,7 +2,7 @@
 
 # CUDA_VISIBLE_DEVICES=0 python main.py --ds site-$1 > site-2/log.txt
 #python main.py --ds site-$1 > site-$1/log.txt
-workspace="workspace_medical_2018_challenge_$3_split"
+workspace="workspace_medical_2018_challenge_baseline_$3"
 
 rm -rf $workspace/site-$1
 mkdir $workspace/
@@ -10,4 +10,6 @@ mkdir $workspace/site-$1
 CUDA_VISIBLE_DEVICES=$2 python main.py \
     --ds site-$1 \
     --workspace $workspace \
-    > $workspace/site-$1/log.txt
+    --model $3 > $workspace/site-$1/log.txt
+
+# sbatch simulate_single.slurm 0 0 bert-base-uncased
