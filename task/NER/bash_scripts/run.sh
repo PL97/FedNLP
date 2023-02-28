ef12c6176436f1a37849b006129181a8e69341b6
PL97 <pengle@126.com>
2023-02-28 12:17:31 -0600

    update bash/slurm scripts

--- task/NER/bash_scripts/run.sh
+++ task/NER/bash_scripts/run.sh
@@ -8,10 +8,9 @@ TF_CPP_MIN_LOG_LEVEL="2" CUDA_VISIBLE_DEVICES=0 python main.py \
     --ds $2 \
     --split $1 \
     --workspace $workspace \
-    --model $3 
-    # > $workspace/$1/log.txt
+    --model $3 > $workspace/$1/log.txt
 
 
 ## use case example
-# sbatch simulate_single.slurm 0 0 bert-base-uncased
-## ./bash_scripts/run.sh site-0 BC2GM bluebert
+# sbatch simulate_single.slurm site-0 BC2GM bluebert
+# ./bash_scripts/run.sh site-0 BC2GM bluebert
