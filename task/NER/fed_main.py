007d1c3269c9da3fe124db835ec84ab36e0a30a2
PL97 <pengle@126.com>
2023-02-28 12:08:26 -0600

    add gpt training instance

--- task/NER/fed_main.py
+++ task/NER/fed_main.py
@@ -6,10 +6,12 @@ from torch.utils.data import DataLoader
 os.environ["TOKENIZERS_PARALLELISM"] = "False"
 
 from models.BERT import BertModel
+from models.GPT import GPTModel
 from datasets.dataset import get_data as get_bert_data
 from datasets.BI_LSTM_dataset import get_data as get_bilstm_crf_data
 from trainer.trainer_bert import NER_FedAvg_bert
 from trainer.trainer_bilstm_crf import NER_FedAvg_bilstm_crf
+from trainer.trainer_gpt import NER_FedAvg_gpt
 import random
 import numpy as np
 from collections import defaultdict
@@ -25,7 +27,8 @@ def parse_args():
     parser.add_argument("--workspace", type=str, help="WORKSPACE folder", default="site-1")
     parser.add_argument("--n_split", type=int, help="WORKSPACE folder", default=2)
     parser.add_argument("--model", type=str, help="specify which model to use: [bert-base-uncased/BI_LSTM_CRF]", default="bert-base-uncased")
-    parser.add_argument("--batch_size", type=str, help="batchsize of train/val/test loader", default=128)
+    parser.add_argument("--batch_size", type=str, help="batchsize of train/val/test loader", default=64)
+    parser.add_argument("--epochs", type=str, help="total training epochs", default=1)
     args = parser.parse_args()
     return args
 
@@ -63,7 +66,31 @@ if __name__ == "__main__":
                 dls=dls,
                 client_weights = [1/num_client]*num_client, 
                 lrs = [5e-5]*num_client, 
-                max_epoches=50, 
+                max_epoches=args['epochs'], 
+                aggregation_freq=1,
+                device=device, 
+                saved_dir = saved_dir,
+                model_name=args['model'],
+                num_labels=num_labels)
+                    
+        fed_model.fit()
+    
+    elif "gpt" in args['model'].lower():
+        ## need to define tokenizer here
+        tokenizer = GPTModel(num_labels = num_labels, model_name=args['model']).tokenizer
+        
+        for idx in range(num_client):    
+            dataset_name = f"site-{idx+1}"
+            df_train = pd.read_csv(os.path.join(root_dir, dataset_name+"_train.csv"))
+            df_val = pd.read_csv(os.path.join(root_dir, dataset_name+"_val.csv"))
+            dls[idx], stats = get_bert_data(df_train=df_train, df_val=df_val, bs=args['batch_size'], tokenizer=tokenizer, df_test=df_test)
+            
+        ## prepare models
+        fed_model = NER_FedAvg_gpt(
+                dls=dls,
+                client_weights = [1/num_client]*num_client, 
+                lrs = [5e-5]*num_client, 
+                max_epoches=args['epochs'], 
                 aggregation_freq=1,
                 device=device, 
                 saved_dir = saved_dir,
@@ -86,7 +113,7 @@ if __name__ == "__main__":
                     dls=dls,
                     client_weights = [1/num_client]*num_client, 
                     lrs = [1e-3]*num_client, 
-                    max_epoches=5, 
+                    max_epoches=args['epochs'], 
                     aggregation_freq=1,
                     device=device, 
                     saved_dir = saved_dir,
@@ -97,7 +124,7 @@ if __name__ == "__main__":
                     )
         fed_model.fit()
     
-    metrics = {"split": fed_model.inference(dls[0][split], stats['ids_to_labels'], split) for split in ['test']}
+    metrics = {split: fed_model.inference(dls[0][split], stats['ids_to_labels'], split) for split in ['test']}
     for split in ['test']:
         pd.DataFrame(metrics[split]['meta']).to_csv(f"{args['workspace']}/{split}_prediction.csv")
         metrics[split].pop('meta')
