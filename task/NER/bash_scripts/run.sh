5cbf248592500eea176a54386a9ae635d5ed2545
PL97 <pengle@126.com>
2023-02-28 12:12:20 -0600

    unit test for ner -> pass

--- task/NER/bash_scripts/run.sh
+++ task/NER/bash_scripts/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-readonly CUDA_DEVICE
-
 workspace="workspace_$2/$3/baseline/"
 
 rm -rf $workspace/$1 && mkdir -p $workspace/$1 || mkdir -p $workspace/$1
@@ -10,7 +8,8 @@ TF_CPP_MIN_LOG_LEVEL="2" CUDA_VISIBLE_DEVICES=0 python main.py \
     --ds $2 \
     --split $1 \
     --workspace $workspace \
-    --model $3 > $workspace/$1/log.txt
+    --model $3 
+    # > $workspace/$1/log.txt
 
 
 ## use case example
