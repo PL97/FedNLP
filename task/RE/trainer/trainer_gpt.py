a6f8de92c5cf15f56966cbe0e3a09106272fa80e
PL97 <pengle@126.com>
2023-03-02 13:26:54 -0600

    fed for NE unit test: -> pass

--- task/RE/trainer/trainer_gpt.py
+++ task/RE/trainer/trainer_gpt.py
@@ -1,9 +1,9 @@
-from trainer.trainer_bert import NER_FedAvg_bert, trainer_bert
+from trainer.trainer_bert import RE_FedAvg_bert, trainer_bert
 from models.GPT import GPTModel
 
 class trainer_gpt(trainer_bert):
     pass
 
-class NER_FedAvg_gpt(NER_FedAvg_bert):
+class RE_FedAvg_gpt(RE_FedAvg_bert):
     def generate_models(self):
         return GPTModel(num_labels = self.num_labels, model_name=self.model_name)
