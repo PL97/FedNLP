92ae01605218c9722df32ec12fa7ebbf0c165054
PL97 <pengle@126.com>
2023-04-02 01:30:08 -0500

    fix a bug: bilstmcrf inference func arg mismatch

--- NER/trainer/trainer_bilstm_crf.py
+++ NER/trainer/trainer_bilstm_crf.py
@@ -104,8 +104,8 @@ class trainer_bilstm_crf(trainer_base):
                                 ids_to_labels=self.ids_to_labels, \
                                 scaler=self.scaler)
 
-    def inference(self, dataloader, prefix):
-        return _shared_validate(model=self.model, \
+    def inference(self, model, dataloader, prefix):
+        return _shared_validate(model=model, \
                                 dataloader=dataloader, \
                                 device=self.device, \
                                 prefix=prefix, \
